d070f8ddc4c0b64eb7149b92e024799124672465
James Black <jmblack@waimea.school.nz>
2025-03-07 15:09:32 +1300

    nnnnnnnnn

--- src/Main.kt
+++ src/Main.kt
@@ -28,11 +28,13 @@ fun main() {
  * Defines the UI and responds to events
  * The app model should be passwd as an argument
  */
-class MainWindow : JFrame(), ActionListener {
+class MainWindow : JFrame(), ActionListener, KeyListener {
 
     // Fields to hold the UI elements
     private lateinit var greetingLabel: JLabel
     private lateinit var helloButton: JButton
+    private lateinit var goodbyeButton: JButton
+    private lateinit var nameText: JTextField
 
     /**
      * Configure the UI and display it
@@ -50,7 +52,7 @@ class MainWindow : JFrame(), ActionListener {
      */
     private fun configureWindow() {
         title = "Kotlin Swing GUI Demo"
-        contentPane.preferredSize = Dimension(600, 350)
+        contentPane.preferredSize = Dimension(600, 600)
         defaultCloseOperation = WindowConstants.EXIT_ON_CLOSE
         isResizable = false
         layout = null
@@ -62,28 +64,79 @@ class MainWindow : JFrame(), ActionListener {
      * Populate the UI with UI controls
      */
     private fun addControls() {
-        greetingLabel = JLabel("Hello, World!")
+        nameText = JTextField("")
+        nameText.bounds = Rectangle(50,200,500,100)
+        nameText.addActionListener(this)
+        nameText.addKeyListener(this)
+        add(nameText)
+
+        helloButton = JButton("Hello!")
+        helloButton.bounds = Rectangle(50,350,225,100)
+        helloButton.addActionListener(this)     // Handle any clicks
+        add(helloButton)
+
+        goodbyeButton = JButton("Goodbye!")
+        goodbyeButton.bounds = Rectangle(325,350,225,100)
+        goodbyeButton.addActionListener(this)
+        add(goodbyeButton)
+
+        greetingLabel = JLabel("Enter in you name then click a button.")
         greetingLabel.horizontalAlignment = SwingConstants.CENTER
         greetingLabel.bounds = Rectangle(50, 50, 500, 100)
         add(greetingLabel)
 
-        helloButton = JButton("Click Me!")
-        helloButton.bounds = Rectangle(50,200,500,100)
-        helloButton.addActionListener(this)     // Handle any clicks
-        add(helloButton)
+
+
     }
 
 
     /**
      * Handle any UI events (e.g. button clicks)
      */
+
     override fun actionPerformed(e: ActionEvent?) {
         when (e?.source) {
+
+            nameText -> {
+                println("Name text changed!")
+            }
+
+            greetingLabel -> {
+                println("Greetings bitch (O.O)")
+            }
+
             helloButton -> {
-                greetingLabel.text = "You clicked the button!"
+                println("Hello Button Pressed!")
+                var name = "stranger"
+                greetingLabel.text = "Hi there ${nameText.text}!"
+                greetingLabel.foreground = Color.GREEN
+            }
+            goodbyeButton -> {
+                println("Goodbye Button Pressed!")
+                greetingLabel.text = "YOUR UGLY ${nameText.text}!"
+                greetingLabel.foreground = Color.RED
             }
         }
     }
 
+    override fun keyTyped(e: KeyEvent?) {
+        println("Key TYPED: ${e?.keyChar}")
+    }
+
+    override fun keyPressed(e: KeyEvent?) {
+        println("Key PRESSED: ${e?.keyCode}!")
+
+        if (e?.keyCode in KeyEvent.VK_ENTER..KeyEvent.VK_Z) {}
+            println("Letter key pressed!")
+        }
+    }
+
+    override fun keyReleased(e: KeyEvent?) {
+        println("Key RELEASED: ${e?.keyCode}!")
+    }
 }
+
+
+
+
 
